ed0dc9c1a9954a4a5fd786aa371ffeae4f58af6d
poonambhapkar34 <poonambhapkar34@gmail.com>
2023-01-23 21:13:20 +0530

    javasript variable

---
+++ javascript/variable.js
@@ -0,0 +1,34 @@
+//  syntax: let/var/const variable_name : value;
+//reserved_keywords variable_name : data type
+ let b; //declaration
+ b = 30;  //defination
+
+let a = 20; //d+d
+//let a ='poonam'  //redeclaration with let keyword is not possible
+let favFruit = 'mango' ;
+
+console.log(favFruit);
+
+// let x = 40;
+
+// x = 40; ----- // hoisting of variable  is not possible with let keyword.
+// let x;
+// console.log(x); ----not possible
+var t; //declaration
+t = 50; //defination
+var favCar = 'miniCup'; //D+D //redeclaration of variable is possible with var keyword 
+console.log(favCar);
+var favCar = 'swift';
+console.log(favCar);
+
+mobNo = 989999999; // define : hoisting of variable: first define variable's value then declare variable
+var mobNo ;  //declare  //  hoisting  possible with var keyword.
+console.log(mobNo);
+
+const myName = 'poonam'; //value reassign with const keywors is not possible
+//myName = 5000;
+console.log(myName);
+//const myName = 'patil' //redeclaration of variable is not possible with const keyword
+// const g = 50;
+//g = 50; //hoisting of variable is not possible with const keyword
+//const g;
